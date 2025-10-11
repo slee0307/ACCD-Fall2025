79a07276ff84f524f7918f7e6dad686723702839
slee0307 <slee233@inside.artcenter.edu>
2025-10-11 15:25:26 -0700

    week4

--- week3/inclass/scripts/scripts.js
+++ week3/inclass/scripts/scripts.js
@@ -3,7 +3,7 @@ let gT2 = document.getElementById("galleryThumb2")
 let gT3 = document.getElementById("galleryThumb3")
 let gT4 = document.getElementById("galleryThumb4")
 let thumbImages = document.querySelectorAll(".gallery-thumb")
-console.log(thumbImages)
+
 let scImage = document.getElementById("showcaseImage")
 let arrRight = document.querySelector(".showcase-arrow-right")
 let arrLeft = document.querySelector(".showcase-arrow-left")
@@ -11,26 +11,27 @@ let arrLeft = document.querySelector(".showcase-arrow-left")
 let currentIndexThumb = 0
 
 function changeImage (_image) {
-    console.log(_image)
     let thumbSrc = _image.src
     scImage.src = thumbSrc
 
     thumbImages.forEach(function(elem){
         elem.parentElement.classList.remove("current-thumb")
     })
+
+    console.log(_image)
     
-    console.log(_image.target)
+    _image.parentElement.classList.add("current-thumb")
     
-    _image.target.parentElement.classList.add("current-thumb") 
+    console.log(Array.from(thumbImages).indexOf(_image))
     
     currentIndexThumb = Array.from(thumbImages).indexOf(_image)
 }
 
-for(let i = 0; i < thumbImages.length; i++) {
-    thumbImages[i].addEventListener("click", function(event){
-        changeImage(event.target)
-    })
-}
+// for(let i = 0; i < thumbImages.length; i++) {
+//     thumbImages[i].addEventListener("click", function(event){
+//         changeImage(event.target)
+//     })
+// }
 
 arrRight.addEventListener("click",function(){
     currentIndexThumb++
@@ -39,7 +40,7 @@ arrRight.addEventListener("click",function(){
     }
     changeImage(thumbImages[currentIndexThumb])
 })
-arrLeft.addEventListener("cliclk",function(){
+arrLeft.addEventListener("click",function(){
     currentIndexThumb--
     if(currentIndexThumb < 0 ) {
         currentIndexThumb = thumbImages.length - 1
@@ -47,7 +48,15 @@ arrLeft.addEventListener("cliclk",function(){
     changeImage(thumbImages[currentIndexThumb])
 })
 
-gT1.addEventListener("click", changeImage)
-gT2.addEventListener("click", changeImage)
-gT3.addEventListener("click", changeImage)
-gT4.addEventListener("click", changeImage)
+gT1.addEventListener("click", (e)=>{
+    changeImage(e.target)
+})
+gT2.addEventListener("click",  (e)=>{
+    changeImage(e.target)
+})
+gT3.addEventListener("click",  (e)=>{
+    changeImage(e.target)
+})
+gT4.addEventListener("click",  (e)=>{
+    changeImage(e.target)
+})
